9b9bd9070d15472e312653936af8ae7d3824738f
seceretrui <1428433559@qq.com>
2020-04-02 10:02:05 +0800

    delete db/migrate

--- src/main/resources/db/migration/V1__Create_user_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE USER
-(
-    ID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    NAME varchar(100),
-    ACCOUNT_ID varchar(50),
-    TOKEN varchar(36),
-    GMT_CREATE bigint,
-    GMT_MODIFIED bigint
-);
